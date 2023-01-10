8468b8ed97ac9f41f06e121e68c5748ff61b9bd3
aatbip <anantabipal2010@gmail.com>
2023-01-10 20:52:50 +0545

    deployment changes

--- api/src/app.ts
+++ api/src/app.ts
@@ -38,27 +38,32 @@ app.use(
   })
 );
 
+app.use("/api", router);
+
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', `${process.env.FRONTEND_URL}`);
+  res.setHeader("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL}`);
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', "true");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
   // Pass to next layer of middleware
   next();
 });
-
-app.use("/api", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("FIND-RECIPE API SAYS HELLO TO YOU!");
