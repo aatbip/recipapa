1a92f228febb43b87d08e8ee3758590eda674d1f
aatbip <anantabipal2010@gmail.com>
2023-01-10 19:49:35 +0545

    added config

--- api/src/app.ts
+++ api/src/app.ts
@@ -38,9 +38,23 @@ app.use(
   })
 );
 
+// Add headers before the routes are defined
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Headers", "true");
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', `${process.env.FRONTEND_URL}`);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', "true");
+
+  // Pass to next layer of middleware
   next();
 });
 
